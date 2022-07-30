722a76f38a34dc4a736145203b9a62948a34cc80
Laman <laman_garayeva@outlook.com>
2022-07-30 20:49:05 +0200

    lastResponse method coming from SerenityRest

--- src/test/java/eu8/spartan/admin/SpartanAdminGetTest.java
+++ src/test/java/eu8/spartan/admin/SpartanAdminGetTest.java
@@ -5,6 +5,7 @@ import net.serenitybdd.junit5.SerenityTest;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
+import static net.serenitybdd.rest.SerenityRest.lastResponse;
 import static org.hamcrest.Matchers.*;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static io.restassured.RestAssured.*;
@@ -34,9 +35,18 @@ public class SpartanAdminGetTest {
                 .auth().basic("admin", "admin")
                 .pathParam("id", 15)
                 .when()
-                .get("/api/spartans/{id}")
-                .then().statusCode(200)
-                .contentType(ContentType.JSON);
+                .get("/api/spartans/{id}");
+
+        // if you send a request using serenity rest, the response object can be obtained from
+        // the method called lastResponse() without saved separately
+        System.out.println("Status code = " + lastResponse().statusCode());
+
+        //print id
+        System.out.println("lastResponse().path(\"id\") = " + lastResponse().path("id"));
+
+        // use json path with lastResponse and get the name
+        String name = lastResponse().jsonPath().getString("name");
+        System.out.println("name = " + name);
     }
 
 }
