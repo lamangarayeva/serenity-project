f6464bd184b4467d610a437d76d419ff49efd638
Laman <laman_garayeva@outlook.com>
2022-07-30 20:26:34 +0200

    serenityRest imports added

--- src/test/java/eu8/spartan/admin/SpartanAdminGetTest.java
+++ src/test/java/eu8/spartan/admin/SpartanAdminGetTest.java
@@ -2,12 +2,14 @@ package eu8.spartan.admin;
 
 import io.restassured.http.ContentType;
 import net.serenitybdd.junit5.SerenityTest;
+import net.serenitybdd.rest.SerenityRest;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 import static org.hamcrest.Matchers.*;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static io.restassured.RestAssured.*;
+import static net.serenitybdd.rest.SerenityRest.given;
 
 @SerenityTest
 public class SpartanAdminGetTest {
@@ -24,6 +26,17 @@ public class SpartanAdminGetTest {
         .when()
                 .get("/api/spartans")
         .then().statusCode(200)
+                .contentType(ContentType.JSON);
+    }
+
+    @Test
+    public void getOneSpartan(){
+        given().accept(ContentType.JSON)
+                .auth().basic("admin", "admin")
+                .pathParam("id", 15)
+                .when()
+                .get("/api/spartans/{id}")
+                .then().statusCode(200)
                 .contentType(ContentType.JSON);
     }
 
