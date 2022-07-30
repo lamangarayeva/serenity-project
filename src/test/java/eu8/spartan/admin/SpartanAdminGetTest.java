01ee843600fa3a611d9bac92422752ce1b0c0503
Laman <laman_garayeva@outlook.com>
2022-07-30 19:55:53 +0200

    first test

--- src/test/java/eu8/spartan/admin/SpartanAdminGetTest.java
+++ src/test/java/eu8/spartan/admin/SpartanAdminGetTest.java
@@ -1,4 +1,28 @@
 package eu8.spartan.admin;
 
+import io.restassured.http.ContentType;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static org.hamcrest.Matchers.*;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static io.restassured.RestAssured.*;
+
 public class SpartanAdminGetTest {
+
+    @BeforeAll
+    public static void init() {
+        baseURI = "http://44.201.107.188:7000";
+    }
+
+    @Test
+    public void getAllSpartan(){
+        given().accept(ContentType.JSON)
+                .auth().basic("admin", "admin")
+        .when()
+                .get("/api/spartans")
+        .then().statusCode(200)
+                .contentType(ContentType.JSON);
+    }
+
 }
