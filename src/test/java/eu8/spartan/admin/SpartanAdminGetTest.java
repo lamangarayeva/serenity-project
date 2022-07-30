a334e8e8d67c275575120418c98dc7e8121664fe
Laman <laman_garayeva@outlook.com>
2022-07-30 21:00:49 +0200

    serenity assertion

--- src/test/java/eu8/spartan/admin/SpartanAdminGetTest.java
+++ src/test/java/eu8/spartan/admin/SpartanAdminGetTest.java
@@ -2,7 +2,9 @@ package eu8.spartan.admin;
 
 import io.restassured.http.ContentType;
 import net.serenitybdd.junit5.SerenityTest;
+import net.serenitybdd.rest.Ensure;
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static net.serenitybdd.rest.SerenityRest.lastResponse;
@@ -47,6 +49,27 @@ public class SpartanAdminGetTest {
         // use json path with lastResponse and get the name
         String name = lastResponse().jsonPath().getString("name");
         System.out.println("name = " + name);
+    }
+
+    @DisplayName("GET request with Serenity Assertion way")
+    @Test
+    public void getOneSpartanAssertion(){
+
+        given().accept(ContentType.JSON)
+                .auth().basic("admin", "admin")
+                .pathParam("id", 15)
+                .when()
+                .get("/api/spartans/{id}");
+
+        // Serenity way of assertion
+
+        Ensure.that("Status code is 200",validatableResponse -> validatableResponse.statusCode(200));
+
+        Ensure.that("Content-type is JSON",validatableResponse -> validatableResponse.contentType(ContentType.JSON));
+
+        Ensure.that("ID is 15", validatableResponse -> validatableResponse.body("id", is(15)));
+
+
     }
 
 }
