65302ee496dd5a2b2253587c489097ab2bc33f83
Laman <laman_garayeva@outlook.com>
2022-07-30 20:16:14 +0200

    serenity

--- src/test/java/eu8/spartan/admin/SpartanAdminGetTest.java
+++ src/test/java/eu8/spartan/admin/SpartanAdminGetTest.java
@@ -1,6 +1,7 @@
 package eu8.spartan.admin;
 
 import io.restassured.http.ContentType;
+import net.serenitybdd.junit5.SerenityTest;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
@@ -8,6 +9,7 @@ import static org.hamcrest.Matchers.*;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static io.restassured.RestAssured.*;
 
+@SerenityTest
 public class SpartanAdminGetTest {
 
     @BeforeAll
